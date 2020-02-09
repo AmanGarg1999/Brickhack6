79cd5362793665eea3e701352360b33aaf945148
Aman garg <amangarg19998@gmail.com>
2020-02-08 23:55:58 -0500

    commit 1.0

--- app/src/main/java/com/example/brickhack6/getInfo.java
+++ app/src/main/java/com/example/brickhack6/getInfo.java
@@ -16,6 +16,10 @@ public class getInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_info);
+
+        search = (EditText)findViewById(R.id.search_edit);
+        search_bttn = (Button) findViewById(R.id.search);
+        back = (Button) findViewById(R.id.back_btn);
     }
 
 }
