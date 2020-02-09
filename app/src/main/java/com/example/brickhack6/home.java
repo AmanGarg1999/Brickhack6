8595b4681f3fe8b191f2eef3d1353a596f0d77be
Aman garg <amangarg19998@gmail.com>
2020-02-09 08:06:22 -0500

    commit 6.0

--- app/src/main/java/com/example/brickhack6/home.java
+++ app/src/main/java/com/example/brickhack6/home.java
@@ -17,6 +17,8 @@ public class home extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
     Button btn;
+    Button view_hist;
+    Button view_est;
 
     private ArrayList<String> mName = new ArrayList<>();
     private ArrayList<String> mImageUrls = new ArrayList<>();
@@ -26,13 +28,35 @@ public class home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        initImageBitmaps();
+        initRecyclerView();
         btn = findViewById(R.id.addToList);
+        view_est = findViewById(R.id.exp_btn);
+        view_hist = findViewById(R.id.hist_btn);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(home.this, getInfo.class);
+                Intent intent = new Intent(home.this, search_result.class);
+                startActivity(intent);
+            }
+        });
+
+        view_est.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(home.this, expense.class);
+                startActivity(intent);
+            }
+        });
+
+        view_hist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(home.this, View_history.class);
                 startActivity(intent);
             }
         });
@@ -41,8 +65,17 @@ public class home extends AppCompatActivity {
     private void initImageBitmaps(){
         Log.d(TAG, "Preparing Bitmaps");
 
-        mImageUrls.add("");
-        mName.add("");
+        mName.add("Rolling Rock 6p/12z btls");
+        mImageUrls.add("https://dpsassetsstorageprod.blob.core.windows.net/images-500/00071439348063.jpg?sv=2018-03-28&si=images-500-AssetSubcriber&sr=c&sig=zv%2BjKOyBk2%2Blc%2BpOhEUoGN5N12le72HSPRxbMsISrcs%3D");
+
+        mName.add("Busch");
+        mImageUrls.add("https://dpsassetsstorageprod.blob.core.windows.net/images-500/018200610479.jpg?sv=2018-03-28&si=images-500-AssetSubcriber&sr=c&sig=zv%2BjKOyBk2%2Blc%2BpOhEUoGN5N12le72HSPRxbMsISrcs%3D");
+
+        mName.add("Corona Light 12pk cans");
+        mImageUrls.add("https://dpsassetsstorageprod.blob.core.windows.net/images-500/080660956213.jpg?sv=2018-03-28&si=images-500-AssetSubcriber&sr=c&sig=zv%2BjKOyBk2%2Blc%2BpOhEUoGN5N12le72HSPRxbMsISrcs%3D");
+
+        mName.add("Premium Vodka");
+        mImageUrls.add("https://dpsassetsstorageprod.blob.core.windows.net/images-500/077890292563.jpg?sv=2018-03-28&si=images-500-AssetSubcriber&sr=c&sig=zv%2BjKOyBk2%2Blc%2BpOhEUoGN5N12le72HSPRxbMsISrcs%3D");
 
     }
 
--- app/src/main/java/com/example/brickhack6/search_result.java
+++ app/src/main/java/com/example/brickhack6/search_result.java
@@ -4,32 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 import com.android.volley.RequestQueue;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
 import java.util.ArrayList;
 
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 
 public class search_result extends AppCompatActivity {
@@ -39,6 +28,7 @@ public class search_result extends AppCompatActivity {
     TextView res;
     String data;
     String line = "";
+    Button add_to_cart;
 
     private ArrayList<String> mName = new ArrayList<>();
     private ArrayList<String> mImageUrls = new ArrayList<>();
@@ -48,12 +38,24 @@ public class search_result extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_result);
 
+        add_to_cart = (Button)findViewById(R.id.ViewCart);
+
+        initImageBitmaps();
+        initRecyclerView();
+
+        add_to_cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(search_result.this, Cart.class);
+                startActivity(intent);
+            }
+        });
         //call_Api();
     }
     private void call_Api() throws IOException {
         RequestQueue queue = Volley.newRequestQueue(this);
 
-        String url = "https://api.wegmans.io/products/search?query={corona}&api-version=2018-10-18&subscription-key=c7b17e30cb674473bbc15e5c0b3ec5dd";
+        //String url = "https://api.wegmans.io/products/search?query={corona}&api-version=2018-10-18&subscription-key=c7b17e30cb674473bbc15e5c0b3ec5dd";
 
         OkHttpClient client = new OkHttpClient();
 
@@ -63,6 +65,19 @@ public class search_result extends AppCompatActivity {
 
     private void initImageBitmaps(){
         Log.d(TAG, "Preparing Bitmaps");
+
+        mName.add("Rolling Rock 6p/12z btls");
+        mImageUrls.add("https://dpsassetsstorageprod.blob.core.windows.net/images-500/00071439348063.jpg?sv=2018-03-28&si=images-500-AssetSubcriber&sr=c&sig=zv%2BjKOyBk2%2Blc%2BpOhEUoGN5N12le72HSPRxbMsISrcs%3D");
+
+        mName.add("Busch");
+        mImageUrls.add("https://dpsassetsstorageprod.blob.core.windows.net/images-500/018200610479.jpg?sv=2018-03-28&si=images-500-AssetSubcriber&sr=c&sig=zv%2BjKOyBk2%2Blc%2BpOhEUoGN5N12le72HSPRxbMsISrcs%3D");
+
+        mName.add("Corona Light 12pk cans");
+        mImageUrls.add("https://dpsassetsstorageprod.blob.core.windows.net/images-500/080660956213.jpg?sv=2018-03-28&si=images-500-AssetSubcriber&sr=c&sig=zv%2BjKOyBk2%2Blc%2BpOhEUoGN5N12le72HSPRxbMsISrcs%3D");
+
+        mName.add("Premium Vodka");
+        mImageUrls.add("https://dpsassetsstorageprod.blob.core.windows.net/images-500/077890292563.jpg?sv=2018-03-28&si=images-500-AssetSubcriber&sr=c&sig=zv%2BjKOyBk2%2Blc%2BpOhEUoGN5N12le72HSPRxbMsISrcs%3D");
+
     }
 
     private void initRecyclerView(){
